b12c4d15144fb14e17fcc93f1a8614fbe8e3bcb1
Charles <60122616+Xhether@users.noreply.github.com>
2024-05-27 14:40:19 -0700

    Before Rotating words under name

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,27 @@
 import Image from "next/image";
 import profilePic from './me.png'
+import React from "react";
+import { ReactTextRotator } from "react-text-rotator";
+
 
 export default function Home() {
-  return (
-    <main className="text-7xl font-bold font-family: -apple-system, BlinkMacSystemFont, sans-serif; flex min-h-screen flex-col items-center justify-between p-24">
-      <Image
-        src={profilePic}
-        alt="Picture of the author"
-        width={620}
-        height={480}
+  var ReactRotatingText = require('react-rotating-text');
 
-      />
-      <p>
-        Charles Liggins
-      </p>
+  return (
+
+    <main className="text-7xl font-bold font-family: -apple-system, BlinkMacSystemFont, sans-serif; flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="space-y-11">
+        <Image
+          src={profilePic}
+          alt="Picture of the author"
+          className="rounded-3xl"
+          style={{ width: '100%', maxHeight: 400, height: '100%', maxWidth: 620 }}
+        />
+        <p>
+          Charles Liggins
+        </p>
+      </div>
+      <ReactRotatingText items={['first', 'second', 'third']} />
     </main >
   );
 }
