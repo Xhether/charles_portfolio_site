deae9db98513979788e5adc47d54d4f8519b05f8
Charles <60122616+Xhether@users.noreply.github.com>
2024-05-27 13:20:15 -0700

    Working pfp code

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
-import Me from "/Users/charlesliggins/Developer/portfolio24/charles_portfolio_site/pictures/me.png";
+import profilePic from './me.png'
 
 export default function Home() {
   return (
     <main className="text-7xl font-bold font-family: -apple-system, BlinkMacSystemFont, sans-serif; flex min-h-screen flex-col items-center justify-between p-24">
-      <img src="/Users/charlesliggins/Developer/portfolio24/charles_portfolio_site/pictures/me.png" alt="Me" />
+      <Image
+        src={profilePic}
+        alt="Picture of the author"
+        width={620}
+        height={480}
+
+      />
       <p>
         Charles Liggins
       </p>
