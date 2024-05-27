50d0365644802d706db6f6752bd58ce4bcc52262
Charles <60122616+Xhether@users.noreply.github.com>
2024-05-27 12:30:34 -0700

    Playing with images

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Me from "/Users/charlesliggins/Developer/portfolio24/charles_portfolio_site/pictures/me.png";
 
 export default function Home() {
   return (
     <main className="text-7xl font-bold font-family: -apple-system, BlinkMacSystemFont, sans-serif; flex min-h-screen flex-col items-center justify-between p-24">
+      <img src="/Users/charlesliggins/Developer/portfolio24/charles_portfolio_site/pictures/me.png" alt="Me" />
       <p>
         Charles Liggins
       </p>
